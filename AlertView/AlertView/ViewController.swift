8d0489115e7d04cf7c47bc6b3f3335a337a21d9e
shima <shima11@users.noreply.github.com>
2016-09-27 16:06:25 +0900

    標準UIAlertControllerの動作テスト

--- AlertView/AlertView/ViewController.swift
+++ AlertView/AlertView/ViewController.swift
@@ -10,16 +10,13 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    let maxTextLenge: Int = 6
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         self.view.backgroundColor = UIColor.lightGrayColor()
         
-//        let alertView = MyAlertView.init()
-//        alertView.backgroundColor = UIColor(white: 0.0, alpha: 0.1)
-//        self.view.addSubview(alertView)
-        
-        // memo: viewdidloadでviewcontroller
     }
     
     override func viewWillAppear(animated: Bool) {
@@ -31,10 +28,10 @@ class ViewController: UIViewController {
     override func viewDidAppear(animated: Bool) {
         super.viewDidAppear(animated)
         //MyAlertViewController.show(presentViewController: self, title: "タイトル", message: "メッセージを\n表示しています。")
-        showAlert()
-        
 
-        
+        showAlert()
+        //showActionSheet()
+
     }
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
@@ -43,9 +40,76 @@ class ViewController: UIViewController {
 
 
     func showAlert() {
+        // alertviewcontroller作成
         let alertViewController = UIAlertController(title: "Alert",
                                           message: "message",
                                           preferredStyle: .Alert)
+        alertViewController.view.backgroundColor = UIColor.redColor()
+        // action 作成
+        let cancelAction: UIAlertAction = UIAlertAction(title: "Cancel",
+                                                        style: UIAlertActionStyle.Cancel,
+                                                        handler: {(action:UIAlertAction!) -> Void in
+                                                            print("Cancel")
+        })
+        let defaultAction: UIAlertAction = UIAlertAction(title: "Default",
+                                                         style: UIAlertActionStyle.Default,
+                                                         handler: {(action: UIAlertAction!) -> Void in
+                                                            print("Default")
+                                                            // textfieldの入力文字を取得
+                                                            if let textFields = alertViewController.textFields as Array<UITextField>? {
+                                                                for textField:UITextField in textFields {
+                                                                    print("\(textField.text)")
+                                                                }
+                                                            }
+        })
+//        let destructiveAction: UIAlertAction = UIAlertAction(title: "Destructive",
+//                                                             style: UIAlertActionStyle.Destructive,
+//                                                             handler: {(action: UIAlertAction!) -> Void in
+//                                                                print("Destructive")
+//        })
+        
+        // actionをalertviewcontrollerに追加
+        alertViewController.addAction(cancelAction)
+        alertViewController.addAction(defaultAction)
+        //alertViewController.addAction(destructiveAction)
+        
+        // textfieldをalertviewcontrollerに追加
+        alertViewController.addTextFieldWithConfigurationHandler({(textField: UITextField) -> Void in
+            textField.placeholder = "user id"
+            textField.keyboardType = .Default
+            textField.delegate = self
+            // notification centerに登録
+//            NSNotificationCenter.defaultCenter().addObserver(self, selector: #selector(ViewController.changeTextFieldText), name: UITextFieldTextDidChangeNotification, object: nil)
+        })
+        alertViewController.addTextFieldWithConfigurationHandler({(textField: UITextField) -> Void in
+            textField.placeholder = "password"
+            textField.secureTextEntry = true
+        })
+        
+        // alertviewcontrollerを画面に表示
+        presentViewController(alertViewController, animated: true, completion: nil)
+    }
+    
+    // textfieldの値が変化した時に呼ばれる (NSNotificationCenter UITextField)
+    func changeTextFieldText(sender: NSNotification) {
+        let textField = sender.object as! UITextField
+        
+        if textField.text?.characters.count < maxTextLenge {
+            textField.enabled = true
+            print (textField.text)
+        }else {
+            textField.enabled = false
+            textField.text = ""
+            print("入力文字数を超えています")
+            // view shakable (ブルブルさせるアニメーション)
+            
+        }
+    }
+    
+    func showActionSheet() {
+        let acshonSheet = UIAlertController(title: "ActoinSheet",
+                                            message: "message",
+                                            preferredStyle: .ActionSheet)
         let cancelAction: UIAlertAction = UIAlertAction(title: "Cancel",
                                                         style: UIAlertActionStyle.Cancel,
                                                         handler: {
@@ -64,12 +128,26 @@ class ViewController: UIViewController {
                                                                 (action: UIAlertAction!) -> Void in
                                                                 print("Destructive")
         })
-        alertViewController.addAction(cancelAction)
-        alertViewController.addAction(defaultAction)
-        alertViewController.addAction(destructiveAction)
+        acshonSheet.addAction(cancelAction)
+        acshonSheet.addAction(defaultAction)
+        acshonSheet.addAction(destructiveAction)
         
-        presentViewController(alertViewController, animated: true, completion: nil)
+        presentViewController(acshonSheet, animated: true, completion: nil)
         
+    }
+    
+}
+
+
+extension ViewController: UITextFieldDelegate {
+    // UITextField.textが変更されたときに呼ばれる
+    func textField(textField: UITextField, shouldChangeCharactersInRange range: NSRange, replacementString string: String) -> Bool {
+        // 入力文字数を制限
+        let text = (textField.text! as NSString).stringByReplacingCharactersInRange(range, withString: string)
+        if text.characters.count > maxTextLenge { return false }
+        print(textField.text)
+
+        return true
     }
 }
 
